3553dfca65cb6684da8a3b9f992d14d93e80ba78
shanwu712 <shanwu712@icloud.com>
2025-04-01 18:00:27 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="flex flex-col h-screen bg-gray-100">
+			<header className="text-4xl font-bold flex py-4 px-8 bg-white">
+				Who is TING HSUAN, WU?
+			</header>
+		</div>
+	);
+}
+
+export default App;
