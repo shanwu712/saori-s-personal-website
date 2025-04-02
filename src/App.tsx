dce47cb2a03a1b8d96895a38d9ad42a891d3dcac
shanwu712 <shanwu712@icloud.com>
2025-04-02 14:15:48 +0800

    core: set up tailwind/shadcn UI/i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useTranslation } from 'react-i18next';
 import './App.css';
 
 function App() {
+	const { t } = useTranslation();
 	return (
 		<div className="flex flex-col h-screen bg-gray-100">
-			<header className="text-4xl font-bold flex py-4 px-8 bg-white">
-				Who is TING HSUAN, WU?
+			<header className="text-4xl font-bold flex py-3 px-8 bg-white">
+				{t('whoAmI')}
 			</header>
+
+			<div className="bg-[url('/bg.JPG')] bg-cover bg-center h-full flex items-center pl-36 ">
+				<p className="text-white text-6xl font-black ">{t('TSW')}</p>
+			</div>
 		</div>
 	);
 }
