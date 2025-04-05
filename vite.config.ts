8abc4bae1c2353dbbd3f2f3529c2373cf406d63e
shanwu712 <shanwu712@icloud.com>
2025-04-05 22:13:39 +0800

    core: the deployment path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/saoris-personal-website/',
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
