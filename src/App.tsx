bf5f90fbf1de93b2916262838ae6e45d2d52b00e
shanwu712 <shanwu712@icloud.com>
2025-04-06 13:27:26 +0800

    fix: try renew the path of bg again

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,12 @@ function App() {
 	}, []);
 	return (
 		<div className="h-screen overflow-y-auto snap-y snap-mandatory bg-gray-100">
-			<div className="bg-[url('./bg.JPG')] bg-cover h-[100vh] flex flex-col snap-start">
+			<div
+				style={{
+					backgroundImage: `url(${import.meta.env.BASE_URL}bg.JPG)`,
+				}}
+				className="bg-cover h-[100vh] flex flex-col snap-start"
+			>
 				<header className="text-4xl justify-between font-black flex py-3 px-8 bg-white">
 					<p>{t('whoAmI')}</p>
 					<LanguagesToggle />
